15161512300010709442b94152cc71e19da8956d
danielle-zn <84985016+danielle-zn@users.noreply.github.com>
2021-07-13 13:15:37 -0600

    Implemented unit conversion with active classes

--- src/index.js
+++ src/index.js
@@ -99,12 +99,16 @@ button.addEventListener("click", getCurrentPosition);
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let farenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(farenheitTemperature);
 }
 
 function displayCeksiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
